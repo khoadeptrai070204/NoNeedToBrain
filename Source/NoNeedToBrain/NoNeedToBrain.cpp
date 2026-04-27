2c417ff78fae399feb713fc464a1698d3a39dba9
khoadeptrai070204 <mkp070204@gmail.com>
2026-04-27 23:58:03 +0700

    clear some files

--- Source/NoNeedToBrain/NoNeedToBrain.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "NoNeedToBrain.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NoNeedToBrain, "NoNeedToBrain" );
-
-DEFINE_LOG_CATEGORY(LogNoNeedToBrain)
--- Source/NoNeedToBrain/NoNeedToBrainGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "NoNeedToBrainGameMode.h"
-
-ANoNeedToBrainGameMode::ANoNeedToBrainGameMode()
-{
-	// stub
-}
